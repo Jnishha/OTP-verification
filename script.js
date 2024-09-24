cf84b0bc891274d1cce99e07fd5c5cb19462331e
Jnishhha <jnishhha@gmail.com>
2024-09-24 22:51:52 +0545

    script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+const inputs = document.querySelectorAll("input"),
+button = document.querySelector("button");
+
+inputs.forEach((input, index1)) =>{
+    input.addEventListener("keyup", () => {
+        const currentInput = input,
+        nextInput = input.nextElementSibling,
+        prevInput =  input.previousElementSibling;
+
+        if (currentInput.value.length > 1) {
+            currentInput.value="";
+            return;
+}
+
+window.addEventListener("load",()=>inputs[0].focus());
